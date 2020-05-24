ab132e5ac89806853c22429a817a9824b611cc74
fincs <fincs@devkitpro.org>
2020-05-24 21:43:33 +0200

    Remove hardware.h, add arm.h to master header

--- include/isan.h
+++ include/isan.h
@@ -7,11 +7,26 @@
 #include "isan/asm.inc"
 #endif
 
-#include "isan/hardware.h"
+// Basic arm definitions
+#include "isan/arm.h"
 
 // arm9 (armv5) platforms: CP15 definitions
 #if __ARM_ARCH == 5
 #include "isan/cp15.h"
+#endif
+
+// Memory map & IO register definitions
+#if defined(IS_GBA)
+#include "isan/gba/mm.h"
+#include "isan/gba/io.h"
+#endif
+
+#if defined(IS_DS7) || defined(IS_DS9)
+// TODO: DS
+#endif
+
+#if defined(IS_3DS9)
+// TODO: 3DS
 #endif
 
 #if !__ASSEMBLER__
