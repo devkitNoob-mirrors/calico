da0b95bb6caedb30821ee7c6618ca3ab5cc246b0
fincs <fincs@devkitpro.org>
2022-12-24 14:21:05 +0100

    nds: move arm9/arm7 source code files to arm9/arm7 folders...

--- source/nds/arm9/mpu_setup.crt0.s
+++ source/nds/arm9/mpu_setup.crt0.s
@@ -10,8 +10,8 @@ FUNC_START32 crt0SetupMPU
 	cmp   r0, #0
 	ldm   r3, {r4-r11}
 
-	orreq r7, r7, #CP15_PU_4M  @ TODO: debug DS
-	orrne r7, r7, #CP15_PU_16M
+	orreq r5, r5, #CP15_PU_4M  @ TODO: debug DS
+	orrne r5, r5, #CP15_PU_16M
 
 	mcr   p15, 0, r4,  c6, c0, 0
 	mcr   p15, 0, r5,  c6, c1, 0
@@ -22,14 +22,14 @@ FUNC_START32 crt0SetupMPU
 	mcr   p15, 0, r10, c6, c6, 0
 	mcr   p15, 0, r11, c6, c7, 0
 
-	mov   r3, #(1<<3)
+	mov   r3, #(1<<1)
 	mcr   p15, 0, r3, c3, c0, 0
-	mov   r3, #(1<<3) | (1<<6)
+	mov   r3, #(1<<1) | (1<<6)
 	mcr   p15, 0, r3, c2, c0, 0
 	mcr   p15, 0, r3, c2, c0, 1
-	ldr   r3, =0x66606000
+	ldr   r3, =0x66600060
 	mcr   p15, 0, r3, c5, c0, 3
-	ldr   r3, =0x06333003
+	ldr   r3, =0x06330033
 	mcr   p15, 0, r3, c5, c0, 2
 
 	mrc   p15, 0, r3, c1, c0, 0
@@ -41,13 +41,13 @@ FUNC_START32 crt0SetupMPU
 	bx    lr
 
 .LmpuRegions:
-	.word CP15_PU_ENABLE | CP15_PU_64M | MM_IO
+	.word CP15_PU_ENABLE | CP15_PU_64M | MM_IO   @ IO + VRAM
+	.word CP15_PU_ENABLE | MM_MAINRAM            @ Main RAM
 	.word 0
 	.word 0
-	.word CP15_PU_ENABLE | MM_MAINRAM
-	.word CP15_PU_ENABLE | CP15_PU_64K | MM_DTCM
-	.word CP15_PU_ENABLE | CP15_PU_32K | MM_ITCM
-	.word CP15_PU_ENABLE | CP15_PU_32K | MM_BIOS
-	.word CP15_PU_ENABLE | ((5-1)<<1)
+	.word CP15_PU_ENABLE | CP15_PU_64K | MM_DTCM @ DTCM + high shared memory
+	.word CP15_PU_ENABLE | CP15_PU_32K | MM_ITCM @ ITCM
+	.word CP15_PU_ENABLE | CP15_PU_32K | MM_BIOS @ BIOS
+	.word CP15_PU_ENABLE | ((5-1)<<1)            @ Exception vectors (in ITCM)
 
 FUNC_END
