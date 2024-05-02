bcdfc8ce2707efffb4e192cc71ed4bf825dec77e
fincs <fincs@devkitpro.org>
2024-05-03 00:22:56 +0200

    arm7_debug: document

--- include/calico/nds/arm9/arm7_debug.h
+++ include/calico/nds/arm9/arm7_debug.h
@@ -5,14 +5,22 @@
 
 #include "../../types.h"
 
-/*! @addtogroup dev
+/*! @addtogroup env
 	@{
 */
 
 MK_EXTERN_C_START
 
+/*! @brief ARM7 debug output callback
+	@param[in] buf Message buffer (not NUL-terminated)
+	@param[in] size Length of the message buffer in bytes
+*/
 typedef void (* Arm7DebugFn)(const char* buf, size_t size);
 
+/*! @brief Starts a server thread for handling the ARM7's debug output
+	@param[in] fn Callback function (see @ref Arm7DebugFn)
+	@param[in] thread_prio Priority of the server thread
+*/
 void installArm7DebugSupport(Arm7DebugFn fn, u8 thread_prio);
 
 MK_EXTERN_C_END
